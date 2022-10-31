1e0eb784d0835f962dec238a4905d697af3753b0
Haader <37Hader38@gmail.com>
2022-10-31 00:29:45 -0300

    creación del local storage

--- logica/calendario.js
+++ logica/calendario.js
@@ -5,52 +5,60 @@
 var Semana=["LUNES","MARTES","MIERCOLES","JUEVES","VIERNES","SABADO"];
 var horarios=["1ra","2da","3ra","4ta"];
 
-//primer numero es el día 
-//segund numero es el turno 0: mañana 1:tarde 2:noche
-let misHorarios=`{
-	"0": {
-		"0": ["-------", "-------", "-------", "-------"],
-		"1": ["-------", "-------", "-------", "-------"],
-		"2": ["-------", "-------", "-------", "-------"]
-	},
-	"1": {
-		"0": ["-------", "-------", "-------", "-------"],
-		"1": ["-------", "-------", "-------", "-------"],
-		"2": ["-------", "-------", "-------", "-------"]
-	},
-  "2": {
-		"0": ["-------", "-------", "-------", "-------"],
-		"1": ["-------", "-------", "-------", "-------"],
-		"2": ["-------", "-------", "-------", "-------"]
-	},
-  "3": {
-		"0": ["-------", "-------", "-------", "-------"],
-		"1": ["-------", "-------", "-------", "-------"],
-		"2": ["-------", "-------", "-------", "-------"]
-	},
-  "4": {
-		"0": ["-------", "-------", "-------", "-------"],
-		"1": ["-------", "-------", "-------", "-------"],
-		"2": ["-------", "-------", "-------", "-------"]
-	},
-  "5": {
-		"0": ["-------", "-------", "-------", "-------"],
-		"1": ["-------", "-------", "-------", "-------"],
-		"2": ["-------", "-------", "-------", "-------"]
-	},
-  "6": {
-		"0": ["-------", "-------", "-------", "-------"],
-		"1": ["-------", "-------", "-------", "-------"],
-		"2": ["-------", "-------", "-------", "-------"]
-	}
-}`
+//primer numero es el *--día--* (0=LUNES,1=MARTES,2=MIERCOLES,3=JUEVES,4=VIERNES,5=SABADO)
+//segund numero es el *--turno--*    (0: mañana     1:tarde      2:noche)
+//tercer numero 0123   <== la posición 
+
+
+//comprobamos si existen datos en LocalStorage
+if(localStorage.getItem('calendarioLocal')==null){
+	var misHorarios=`{
+		"0": {
+			"0": ["-------", "-------", "-------", "-------"],
+			"1": ["-------", "-------", "-------", "-------"],
+			"2": ["-------", "-------", "-------", "-------"]
+		},
+		"1": {
+			"0": ["-------", "-------", "-------", "-------"],
+			"1": ["-------", "-------", "-------", "-------"],
+			"2": ["-------", "-------", "-------", "-------"]
+		},
+	  "2": {
+			"0": ["-------", "-------", "-------", "-------"],
+			"1": ["-------", "-------", "-------", "-------"],
+			"2": ["-------", "-------", "-------", "-------"]
+		},
+	  "3": {
+			"0": ["-------", "-------", "-------", "-------"],
+			"1": ["-------", "-------", "-------", "-------"],
+			"2": ["-------", "-------", "-------", "-------"]
+		},
+	  "4": {
+			"0": ["-------", "-------", "-------", "-------"],
+			"1": ["-------", "-------", "-------", "-------"],
+			"2": ["-------", "-------", "-------", "-------"]
+		},
+	  "5": {
+			"0": ["-------", "-------", "-------", "-------"],
+			"1": ["-------", "-------", "-------", "-------"],
+			"2": ["-------", "-------", "-------", "-------"]
+		},
+	  "6": {
+			"0": ["-------", "-------", "-------", "-------"],
+			"1": ["-------", "-------", "-------", "-------"],
+			"2": ["-------", "-------", "-------", "-------"]
+		}
+	}`
+}else{
+	var misHorarios=localStorage.getItem('calendarioLocal');
+}
 
 //parceamos
 var calendario=JSON.parse(misHorarios);
 
 
 
-//renderizarCalendario
+//renderizamos todos el calendario 
 renderizarCalendarioAll();
 function renderizarCalendarioAll(){
         let cuerpo=document.getElementById('cuerpoCalendario');//cuerpoCalendario es el cuerpo del calendario
@@ -61,6 +69,7 @@ function renderizarCalendarioAll(){
 let cont=0;
 cuerpo.innerHTML="";
 let color="turnoMN";
+let btnEstado="btnOff";
 for (let turno = 0; turno < 3; turno++) {
 				
 				for (let hora = 0; hora < 4; hora++) {
@@ -74,8 +83,15 @@ for (let turno = 0; turno < 3; turno++) {
 					cuerpo.innerHTML+=`<tr class="${color}" id="${cont}"></tr>`;
 						
 						Semana.forEach((dia,diaNum)=>{
+
+							if(calendario[diaNum][turno][hora]=="Ocupado"){
+								btnEstado="btnOn";
+							}else{
+								btnEstado="btnOff";
+							}
+
 							document.getElementById(cont).innerHTML+=`
-									<td><button class="btnOff" id="id${diaNum}_${turno}_${hora}" onclick="action(${diaNum},${turno},${hora})">${calendario[diaNum][turno][hora]}</button></td>
+									<td><button class=${btnEstado} id="id${diaNum}_${turno}_${hora}" onclick="action(${diaNum},${turno},${hora})">${calendario[diaNum][turno][hora]}</button></td>
 									`;
 						})
 
@@ -87,15 +103,19 @@ for (let turno = 0; turno < 3; turno++) {
 			cont=0;
 }
 
+//función para guardar datos en el array CALENDARIO
 function action(diaNum,turno,hora){
 
+	//primero lee los datos del array CALENDARIO
+	//si el valor es "OCUPADO" guarda "-------"
+	//luego renderiza
 	if(calendario[diaNum][turno][hora]=="Ocupado"){
 
 		calendario[diaNum][turno][hora]='-------';
 		renderizarCalendarioOne(diaNum,turno,hora);
 		document.getElementById("id"+diaNum+"_"+turno+"_"+hora).setAttribute("class","btnOff");		
 		//document.getElementById("id"+diaNum+"_"+turno+"_"+hora).style.backgroundColor='rgb(56, 200, 168)';
-
+		guardarDatos(calendario);
 	}else{
 
 		calendario[diaNum][turno][hora]='Ocupado';
@@ -103,7 +123,7 @@ function action(diaNum,turno,hora){
 		document.getElementById("id"+diaNum+"_"+turno+"_"+hora).setAttribute("class","btnOn");
 		
 		//document.getElementById("id"+diaNum+"_"+turno+"_"+hora).style.backgroundColor='rgb(176, 64, 168)';
-
+		guardarDatos(calendario);
 	}
 	
 }
@@ -126,4 +146,8 @@ function pushHorario(){
 //deleteHorario
 function deleteHorario(){
 
+}
+
+function guardarDatos(dato){
+	localStorage.setItem("calendarioLocal",JSON.stringify(dato));
 }
