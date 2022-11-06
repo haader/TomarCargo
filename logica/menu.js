521699027f534b2db1c416bc01797cc383810eb4
Haader <37Hader38@gmail.com>
2022-11-05 22:34:32 -0300

    margin contenedor 7rem y 0rem

--- logica/menu.js
+++ logica/menu.js
@@ -7,8 +7,11 @@ document.getElementById("IDicon").addEventListener("click",()=>{
     if(document.getElementById("idListDesplegableCel").style.display=="none"){
         
         document.getElementById("idListDesplegableCel").style.display="flex";
+
+        document.getElementById("IDhorarioContenedor").style.marginTop="7.5rem";
     }else{
         
         document.getElementById("idListDesplegableCel").style.display="none";
+        document.getElementById("IDhorarioContenedor").style.marginTop="0";
     }
 });
