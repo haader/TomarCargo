7a49eff1a2b6d36dc3b30140c3f2009e29fd14d5
Haader <37Hader38@gmail.com>
2022-11-05 21:47:01 -0300

    cambiando la ventana de los db localsorage

--- logica/windowsDato.js
+++ logica/windowsDato.js
@@ -1,15 +1,31 @@
 function irConfDato(){
     
+    //estado CALENDARIO
     if(!localStorage.getItem("calendarioLocal")){
         var estadoHorarioDB="NO HAY Datos Guardados";
     }else{
         var estadoHorarioDB="HAY Datos Guardados";
     }
 
+    //estdo FILTRO
     if(!localStorage.getItem("objetoSeleccionCargos")){
         var estadoFiltroDB="NO HAY Datos Guardados";
     }else{
         var estadoFiltroDB="HAY Datos Guardados";
+    }
+
+    //estado arrayDistrito
+    if(!localStorage.getItem("arrayDistrito")){
+        var estadoDistritoDB="NO HAY Datos Guardados";
+    }else{
+        var estadoDistritoDB="HAY Datos Guardados";
+    }
+
+    //estado arrayNivel
+    if(!localStorage.getItem("arrayNivel")){
+        var estadoNivelDB="NO HAY Datos Guardados";
+    }else{
+        var estadoNivelDB="HAY Datos Guardados";
     }
 
     document.getElementById("cuerpo").innerHTML+=`
@@ -21,15 +37,26 @@ function irConfDato(){
             <div class="divFiltro" id="IDHorarioDB">
                 <text><strong>Horarios:</strong></text>
                 <p>${estadoHorarioDB}</p>
-                <button  id="btnDBHorario" onclick="deleteDBHorario()">Borrar Datos</button>    
+                <button  onclick="deleteDBHorario()">Borrar Datos</button>    
             </div>
 
 
             <div class="divFiltro" id="IDFiltroDB">
             <text><strong>Filtros:</strong></text>
                 <p>${estadoFiltroDB}</p>
-                <button  id="btnDBFiltro" onclick="deleteDBfiltro()">Borrar Datos</button>    
+                <button  onclick="deleteDBfiltro()">Borrar Datos</button>    
+            </div>
 
+            <div class="divFiltro" id="IDFiltroDB">
+            <text><strong>Filtros:</strong></text>
+                <p>${estadoDistritoDB}</p>
+                <button onclick="deleteDBdistrito()">Borrar Datos</button>     
+            </div>
+
+            <div class="divFiltro" id="IDFiltroDB">
+            <text><strong>Filtros:</strong></text>
+                <p>${estadoNivelDB}</p>
+                <button  onclick="deleteDBnivel()">Borrar Datos</button>    
             </div>
     </div>
     <div id="blackID" class="black">
@@ -46,7 +73,7 @@ function deleteDBHorario(){
     
     <text><strong>Horarios:</strong></text>
     <p>${estadoHorarioDB}</p>
-    <button  id="btnDBHorario" onclick="deleteDBHorario()">Borrar Datos</button>    
+    <button  onclick="deleteDBHorario()">Borrar Datos</button>    
     `;
 }
 
@@ -57,10 +84,32 @@ function deleteDBfiltro(){
     
                                                 <text><strong>Filtros:</strong></text>
                                                 <p>${estadoFiltroDB}</p>
-                                                <button  id="btnDBFiltro" onclick="deleteDBfiltro()">Borrar Datos</button>   
+                                                <button  onclick="deleteDBfiltro()">Borrar Datos</button>   
                                                 
                                                 `
 
-    
-    
 }
+
+
+function deleteDBdistrito(){
+    localStorage.removeItem("arrayDistrito");
+    estadoFiltroDB="Los datos fueron borrados";
+    document.getElementById("IDDistritoDB").innerHTML=`
+    
+                    <text><strong>Filtros:</strong></text>
+                    <p>${estadoDistritoDB}</p>
+                    <button onclick="deleteDBdistrito()">Borrar Datos</button>   
+                    `
+}
+
+function deleteDBnivel(){
+    localStorage.removeItem("arrayNivel");
+    estadoFiltroDB="Los datos fueron borrados";
+    document.getElementById("IDNivelDB").innerHTML=`
+    
+                    <text><strong>Filtros:</strong></text>
+                    <p>${estadoNivelDB}</p>
+                    <button  onclick="deleteDBnivel()">Borrar Datos</button>   
+                    `
+}
+
